3a97189adcf5ee657f53c9b0acae6b2c08431e49
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-06-27 13:11:49 -0300

    livros do alurabooks

---
+++ app/metodoForEach.js
@@ -0,0 +1,27 @@
+// Nosso desafio agora é pegarmos as informações do json, que a colocamos na nossa variável de livros, e exibi-las na tela. 
+// let livros = ['JS', 'Python', 'Go'];
+// for (let i = 0; i < livros.length; i++) {
+//          console.log(livros[i]);
+//      }  --> essa forma é muito verbosa para conseguir o resultado que queremos, então vamos usar o forEach().
+
+const elementoParaInserirLivros = document.getElementById('livros');
+
+function exibirOsLivrosNatela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+            <div class="livro">
+                <img class="livro__imagens" src="${livro.imagem}"
+                    alt="${livro.alt}" 
+                />
+                <h2 class="livro__titulo">
+                   ${livro.titulo}
+                </h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">${livro.preco}</p>
+                <div class="tags">
+                    <span class="tag">${livro.categoria}</span>
+                </div>
+            </div>
+        `;
+    });
+};

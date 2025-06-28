f0e8c94184e3f71a4be94003c8d0fc8dbac2b8bc
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-06-27 21:26:58 -0300

    Filtrado os livros disponíveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,7 +6,11 @@ botoes.forEach(botao => botao.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
     const elementoBotao = document.getElementById(this.id);
     const categoria = elementoBotao.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? 
+        livros.filter(livro => livro.categoria == categoria) : 
+        livros.filter(livro => livro.quantidade > 0);
+        // Dessa forma, primeiro ele verifica as categorias que existem, depois ele confere se eles possuem a quantidade acima de 0.
+        
     // console.table(livrosFiltrados);
     exibirOsLivrosNaTela(livrosFiltrados);
 };
